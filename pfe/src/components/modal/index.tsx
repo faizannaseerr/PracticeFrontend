954479e4f71e647a317e33e391a7377740ef582f
Faizan Naseer <f.naseer@mail.utoronto.ca>
2025-02-16 02:49:06 -0500

    added cursor movement

--- pfe/src/components/modal/index.tsx
+++ pfe/src/components/modal/index.tsx
@@ -15,39 +15,58 @@ const index = ({ modal, projects }: {
 }) => {
     const { active, index } = modal;
     const container = useRef<HTMLDivElement>(null);
+    const cursor = useRef<HTMLDivElement>(null);
+    const cursorText = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         const moveContainerX = gsap.quickTo(container.current, "left", { duration: 0.8, ease: "power3" });
         const moveContainerY = gsap.quickTo(container.current, "top", { duration: 0.8, ease: "power3" });
 
+        const moveCursorX = gsap.quickTo(cursor.current, "left", { duration: 0.5, ease: "power3" });
+        const moveCursorY = gsap.quickTo(cursor.current, "top", { duration: 0.5, ease: "power3" });
+
+        const moveCursorTextX = gsap.quickTo(cursorText.current, "left", { duration: 0.45, ease: "power3" });
+        const moveCursorTextY = gsap.quickTo(cursorText.current, "top", { duration: 0.45, ease: "power3" });
+
         window.addEventListener("mousemove", (e) => {
             moveContainerX(e.clientX);
             moveContainerY(e.clientY);
+            moveCursorX(e.clientX);
+            moveCursorY(e.clientY);
+            moveCursorTextX(e.clientX);
+            moveCursorTextY(e.clientY);
         })
     }, [])
     return (
-        <motion.div ref={container} variants={scaleAnimation} initial="initial" animate={active ? "open" : "closed"}
-            className='flex items-center justify-center absolute h-[300px] w-[350px] overflow-hidden pointer-events-none'>
-            <div className='absolute h-full w-full transition-all duration-300' style={{ top: index * -100 + "%" }}>
-                {
-                    projects.map((project, index) => {
-                        const { src, colour } = project;
-                        return (
-                            <div className='h-full flex items-center justify-center'
-                                key={`modal_${index}`} style={{ backgroundColor: colour }}>
-                                <Image
-                                    style={{ height: 'auto' }}
-                                    src={`/images/${src}`}
-                                    height={0}
-                                    width={300}
-                                    alt="picture"
-                                />
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </motion.div>
+        <>
+            <motion.div ref={container} variants={scaleAnimation} initial="initial" animate={active ? "open" : "closed"}
+                className='flex items-center justify-center absolute h-[300px] w-[350px] overflow-hidden pointer-events-none'>
+                <div className='absolute h-full w-full transition-all duration-300' style={{ top: index * -100 + "%" }}>
+                    {
+                        projects.map((project, index) => {
+                            const { src, colour } = project;
+                            return (
+                                <div className='h-full flex items-center justify-center'
+                                    key={`modal_${index}`} style={{ backgroundColor: colour }}>
+                                    <Image
+                                        style={{ height: 'auto' }}
+                                        src={`/images/${src}`}
+                                        height={0}
+                                        width={300}
+                                        alt="picture"
+                                    />
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </motion.div>
+            <motion.div ref={cursor} variants={scaleAnimation} initial="initial" animate={active ? "open" : "closed"}
+                className='w-20 h-20 bg-[#455CE9] absolute pointer-events-none rounded-full flex items-center justify-center'>
+            </motion.div>
+            <motion.div ref={cursorText} variants={scaleAnimation} initial="initial" animate={active ? "open" : "closed"}
+                className='text-white bg-transparent absolute pointer-events-none'>View</motion.div>
+        </>
     )
 }
 
